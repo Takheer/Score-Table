6d3c37e537da79fb73314e0311362a67387137f0
Takheer <takheer228@gmail.com>
2015-10-05 21:01:49 +0500

    Fixed bug with overflow of score view

--- app/src/main/java/ru/kekes/scoretable/MainActivity.java
+++ app/src/main/java/ru/kekes/scoretable/MainActivity.java
@@ -120,7 +120,6 @@ public class MainActivity extends AppCompatActivity {
             int leftScoreOnes = leftScore % 10;
 
             switch (leftScoreOnes) {
-                case 0: {leftScoreOnesView.setImageResource(R.drawable.ic_num_0); break;}
                 case 1: {leftScoreOnesView.setImageResource(R.drawable.ic_num_1); break;}
                 case 2: {leftScoreOnesView.setImageResource(R.drawable.ic_num_2); break;}
                 case 3: {leftScoreOnesView.setImageResource(R.drawable.ic_num_3); break;}
@@ -130,10 +129,10 @@ public class MainActivity extends AppCompatActivity {
                 case 7: {leftScoreOnesView.setImageResource(R.drawable.ic_num_7); break;}
                 case 8: {leftScoreOnesView.setImageResource(R.drawable.ic_num_8); break;}
                 case 9: {leftScoreOnesView.setImageResource(R.drawable.ic_num_9); break;}
+                default: {leftScoreOnesView.setImageResource(R.drawable.ic_num_0); break;}
             }
 
             switch (leftScoreTens) {
-                case 0: {leftScoreTensView.setImageResource(R.drawable.ic_num_0); break;}
                 case 1: {leftScoreTensView.setImageResource(R.drawable.ic_num_1); break;}
                 case 2: {leftScoreTensView.setImageResource(R.drawable.ic_num_2); break;}
                 case 3: {leftScoreTensView.setImageResource(R.drawable.ic_num_3); break;}
@@ -143,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
                 case 7: {leftScoreTensView.setImageResource(R.drawable.ic_num_7); break;}
                 case 8: {leftScoreTensView.setImageResource(R.drawable.ic_num_8); break;}
                 case 9: {leftScoreTensView.setImageResource(R.drawable.ic_num_9); break;}
+                default: {leftScoreTensView.setImageResource(R.drawable.ic_num_0); break;}
             }
         } else if (view == findViewById(R.id.rightMinus)){
             ImageView rightScoreTensView = (ImageView) findViewById(R.id.rightTens);
@@ -153,7 +153,6 @@ public class MainActivity extends AppCompatActivity {
             int rightScoreOnes = rightScore % 10;
 
             switch (rightScoreOnes) {
-                case 0: {rightScoreOnesView.setImageResource(R.drawable.ic_num_0); break;}
                 case 1: {rightScoreOnesView.setImageResource(R.drawable.ic_num_1); break;}
                 case 2: {rightScoreOnesView.setImageResource(R.drawable.ic_num_2); break;}
                 case 3: {rightScoreOnesView.setImageResource(R.drawable.ic_num_3); break;}
@@ -163,10 +162,10 @@ public class MainActivity extends AppCompatActivity {
                 case 7: {rightScoreOnesView.setImageResource(R.drawable.ic_num_7); break;}
                 case 8: {rightScoreOnesView.setImageResource(R.drawable.ic_num_8); break;}
                 case 9: {rightScoreOnesView.setImageResource(R.drawable.ic_num_9); break;}
+                default: {rightScoreOnesView.setImageResource(R.drawable.ic_num_0); break;}
             }
 
             switch (rightScoreTens) {
-                case 0: {rightScoreTensView.setImageResource(R.drawable.ic_num_0); break;}
                 case 1: {rightScoreTensView.setImageResource(R.drawable.ic_num_1); break;}
                 case 2: {rightScoreTensView.setImageResource(R.drawable.ic_num_2); break;}
                 case 3: {rightScoreTensView.setImageResource(R.drawable.ic_num_3); break;}
@@ -176,6 +175,7 @@ public class MainActivity extends AppCompatActivity {
                 case 7: {rightScoreTensView.setImageResource(R.drawable.ic_num_7); break;}
                 case 8: {rightScoreTensView.setImageResource(R.drawable.ic_num_8); break;}
                 case 9: {rightScoreTensView.setImageResource(R.drawable.ic_num_9); break;}
+                default: {rightScoreTensView.setImageResource(R.drawable.ic_num_0); break;}
             }
         }
     }
